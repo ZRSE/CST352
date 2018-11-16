4bb05b17106261874c996f245829bd419d94893d
Zakarias Roese <zroese@csumb.edu>
2018-11-16 03:58:26 +0000

    lab_7

---
+++ labs/lab_7/guessNumber/script.js
@@ -0,0 +1,99 @@
+            
+            
+            var randomNumber;
+            randomNumber = Math.floor(Math.random() * 99) + 1;
+            console.log(randomNumber);
+            document.getElementById("numberToGuess").innerHTML = randomNumber;
+            
+            var guessCount = 1;
+            var resetButton = document.querySelector('#reset');
+            resetButton.style.display="none";
+            
+            var guesses = document.querySelector('#guesses');
+            var lastResult = document.querySelector('#lastResult');
+            var lowOrHi = document.querySelector('#lowOrHi');
+           
+            var guessSubmit = document.querySelector('.guessSubmit');
+            var guessField = document.querySelector('.guessField');
+            
+            
+        
+
+            guessSubmit.addEventListener('click', function checkGuess() {
+              
+               var userGuess = Number(guessField.value);
+               
+               if (userGuess > 99 || userGuess < 1) {
+                    $("#lastResult").html("Choose a number between 1 and 99");
+                    $("#lowOrHi").html("");
+                    return false;
+               }
+               
+               if (guessCount === 1) {
+                    $("#guesses").html("Previous guesses: ");
+
+               }
+               
+               guesses.innerHTML += userGuess + ' ';
+               
+               if (userGuess === randomNumber) {
+                    $("#lastResult").html("Congrats! You got it right!");
+                    lastResult.style.backgroundColor = "green";
+                    $("#lowOrHi").html("");
+                    setGameOver();
+               } 
+               else if(guessCount === 7) {
+                   lastResult.innerHTML = 'Sorry, you lost!';
+                   setGameOver();
+               }
+               else {
+                    lastResult.innerHTML = 'Wrong!';
+                    lastResult.style.backgroundColor = "red";
+                    
+                    if (userGuess < randomNumber) {
+                        lowOrHi.innerHTML = 'Last guess was too low!';
+                    } else if (userGuess > randomNumber) {
+                        lowOrHi.innerHTML = 'Last guess was too high!';
+                    }
+               }
+               
+               guessCount++;
+               guessField.value = '';
+               guessField.focus();
+               
+            }
+            );
+            
+            
+            
+            
+            function setGameOver() {
+                guessField.disabled = true;
+                guessSubmit.disabled = true;
+                resetButton.style.display = 'inline';
+                resetButton.addEventListener('click', resetGame);
+            }
+            
+            function resetGame() {
+                guessCount = 1;
+                
+                var resetParas = document.querySelectorAll('.resultParas p');
+                
+                for (var i = 0; i < resetParas.length; i++) {
+                    resetParas[i].textContent = '';
+                }
+                
+                resetButton.style.display = 'none';
+                
+                guessField.disabled = false;
+                guessSubmit.disabled = false;
+                guessField.value = '';
+                guessField.focus();
+                
+                lastResult.style.backgroundColor = 'white';
+                
+                randomNumber = Math.floor(Math.random() * 99) + 8;
+                console.log(randomNumber);
+                document.getElementById("numberToGuess").innerHTML = randomNumber;
+
+            }
